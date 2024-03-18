6281513a21b5752cf62f90c63245edf1d2064bfd
DimachDim <dakuznecov45@gmail.com>
2024-03-19 02:52:30 +0500

    Доьавление кнопок в NoLogin

--- src/components/NoLogined/NoLogined.js
+++ src/components/NoLogined/NoLogined.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Routes, BrowserRouter, Route, Link } from 'react-router-dom';
+import { Button } from "react-bootstrap";
 
 export default function NoLogined(){
     return(
         <BrowserRouter>
-            <Link to={'/'}>Login</Link>
-            <Link to={'/reg'}>Login</Link>
+            <Link to={'/'}><Button>Login</Button></Link>
+            <Link to={'/reg'}><Button>Login</Button></Link>
             <Routes>
                 <Route path='/' element={'login'}/>
                 <Route path='/reg' element={'reg'}/>
--- src/components/NoLogined/Registration.js
+++ src/components/NoLogined/Registration.js
@@ -3,6 +3,7 @@ import React from "react";
 export default function Registration(){
     return(
         <>
+            reg
         </>
     )
 }
