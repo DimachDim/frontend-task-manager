4c6d59ae7c1bf50059018940a8255cb92d046e28
DimachDim <dakuznecov45@gmail.com>
2024-03-19 02:56:45 +0500

    Настройка noLogined

--- src/components/NoLogined/NoLogined.js
+++ src/components/NoLogined/NoLogined.js
@@ -2,14 +2,27 @@ import React from "react";
 import { Routes, BrowserRouter, Route, Link } from 'react-router-dom';
 import { Button } from "react-bootstrap";
 
+// Компоненты
+import Login from "./Login";
+import Registration from "./Registration";
+
+
 export default function NoLogined(){
     return(
         <BrowserRouter>
-            <Link to={'/'}><Button>Login</Button></Link>
-            <Link to={'/reg'}><Button>Login</Button></Link>
+            {/* Ссылки */}
+            <Link to={'/'}>
+                <Button>Login</Button>
+            </Link>
+            
+            <Link to={'/reg'}>
+                <Button>Registration</Button>
+            </Link>
+            
+            {/* Представления */}
             <Routes>
-                <Route path='/' element={'login'}/>
-                <Route path='/reg' element={'reg'}/>
+                <Route path='/' element={<Login/>}/>
+                <Route path='/reg' element={<Registration/>}/>
             </Routes>
         </BrowserRouter> 
     )
