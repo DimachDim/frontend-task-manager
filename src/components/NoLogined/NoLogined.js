f18afeab08bc7d2443c6e4e060f00aff26393ee1
DimachDim <dakuznecov45@gmail.com>
2024-03-19 03:11:24 +0500

    Стилизация переключения Login и Reg

--- src/components/NoLogined/NoLogined.js
+++ src/components/NoLogined/NoLogined.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import { Routes, BrowserRouter, Route, Link } from 'react-router-dom';
 import { Button } from "react-bootstrap";
 
@@ -8,17 +9,23 @@ import Registration from "./Registration";
 
 
 export default function NoLogined(){
+
+    // Стилизация переключения кнопок
+    const [link, setLink] = useState('login');
+    const checkLogin = link === 'login' ? 'success' :"secondary";
+    const checkReg = link === 'reg' ? 'success' :"secondary";
+
     return(
         <div className="position-absolute top-50 start-50 translate-middle">
 
             <BrowserRouter>
                 {/* Ссылки */}
                 <Link to={'/'}>
-                    <Button variant="">Login</Button>
+                    <Button variant={checkLogin} onClick={()=>setLink('login')}>Login</Button>
                 </Link>
                 
                 <Link to={'/reg'}>
-                    <Button>Registration</Button>
+                    <Button variant={checkReg} onClick={()=>setLink('reg')}>Registration</Button>
                 </Link>
                 
                 {/* Представления */}
