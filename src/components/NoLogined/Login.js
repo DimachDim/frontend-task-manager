3f47f77fed60186274f29e64323d918ec507384f
DimachDim <dakuznecov45@gmail.com>
2024-03-19 11:46:02 +0500

    Добавление форм в Login и Reg

--- src/components/NoLogined/Login.js
+++ src/components/NoLogined/Login.js
@@ -1,9 +1,23 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 export default function Login(){
     return(
-        <>
-            login
-        </>
+        <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Имя пользователя</Form.Label>
+                <Form.Control type="username" placeholder="User name" />
+            </Form.Group>
+  
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Пароль</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+        
+            <Button variant="primary" type="submit">
+                Войти
+            </Button>
+        </Form>
     )
 }
--- src/components/NoLogined/Registration.js
+++ src/components/NoLogined/Registration.js
@@ -1,9 +1,33 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 export default function Registration(){
     return(
-        <>
-            reg
-        </>
+        <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Токен регистрации</Form.Label>
+                <Form.Control type="token-reg" placeholder="Token" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Придумайте Имя</Form.Label>
+                <Form.Control type="username" placeholder="User name" />
+            </Form.Group>
+  
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Пароль</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Повторите пароль</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+        
+            <Button variant="primary" type="submit">
+                Войти
+            </Button>
+        </Form>
     )
 }
