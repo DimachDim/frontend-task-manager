ea0f933fa64f240a27fa75bdd14a82367fa0c02e
DimachDim <dakuznecov45@gmail.com>
2024-03-19 12:47:35 +0500

    Login добавление ссылок на поля

--- src/components/NoLogined/Login.js
+++ src/components/NoLogined/Login.js
@@ -4,16 +4,21 @@ import Form from 'react-bootstrap/Form';
 
 
 export default function Login(){
+
+    // Ссылки на поля формы
+    const refUsername = React.createRef();
+    const refPassword = React.createRef();
+
     return(
         <Form>
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Имя пользователя</Form.Label>
-                <Form.Control type="username" placeholder="User name" />
+                <Form.Control type="username" placeholder="User name" ref={refUsername}/>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Пароль</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control type="password" placeholder="Password" ref={refPassword}/>
             </Form.Group>
         
             <Button variant="primary">
