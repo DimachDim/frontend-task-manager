6758f11b5705cb16a2b156f356f986660f6aa14f
DimachDim <dakuznecov45@gmail.com>
2024-03-26 16:09:27 +0500

    Стилизация списка пользователей

--- src/components/Logined/User/UsersList.js
+++ src/components/Logined/User/UsersList.js
@@ -5,7 +5,7 @@ export default function UsersList(props){
     // Перебираем массив 
     const usresComp = props.users.map((item, index)=>{
         return(
-            <div key={index}>
+            <div key={index} className='list-group-item'>
                 {item.userName}
             </div>
         )
@@ -13,8 +13,12 @@ export default function UsersList(props){
 
     console.log('props', props)
     return(
-        <>
-            {usresComp}
-        </>
+        <div className="container mt-5">
+            <div className="row">
+                <div className="list-group">
+                    {usresComp}
+                </div>
+            </div>
+        </div>
     )
 }
