f2184425b5d8ecc5d02c3e6bb34537ddfb1180e3
DimachDim <dakuznecov45@gmail.com>
2024-03-27 21:14:53 +0500

    Написание PendingTasks

--- src/components/Logined/Tasks/ReadTask/ListTask.js
+++ src/components/Logined/Tasks/ReadTask/ListTask.js
@@ -8,24 +8,28 @@ export default function ListTask(props){
     const arrTasks = props.arrData;             // Массив данных о задачах
     let arrComponentTasks;                      // Будет хранить массив компонентов
 
-    // Преобразование массива в массив компонентов
-    arrComponentTasks = arrTasks.map((item, index) =>{
-    
-        return (
-            <div className="col-sm-12 col-md-6 col-lg-4 mt-3"  key={index}>
-                <ContainerTask
-                    key={index}
-                    id={item.id}
-                    title={item.title}
-                    text={item.text}
-                    token={item.token}
-                    userName={item.userName}
+    // Если arrTasks является массивом
+    if(Array.isArray(arrTasks)){
 
-                    refreshPerent={props.refreshPerent    /* Функция обнавления */}
-                />
-            </div>
-        )
-    }) 
+        // Преобразование массива в массив компонентов
+        arrComponentTasks = arrTasks.map((item, index) =>{
+            
+            return (
+                <div className="col-sm-12 col-md-6 col-lg-4 mt-3"  key={index}>
+                    <ContainerTask
+                        key={index}
+                        id={item.id}
+                        title={item.title}
+                        text={item.text}
+                        token={item.token}
+                        userName={item.userName}
+                        
+                        refreshPerent={props.refreshPerent    /* Функция обнавления */}
+                        />
+                </div>
+            )
+       }) 
+    }
     
 
     return(
--- src/components/Logined/Tasks/ReadTask/PendingTasks/PendingTasks.js
+++ src/components/Logined/Tasks/ReadTask/PendingTasks/PendingTasks.js
@@ -1,9 +1,32 @@
 import React from "react";
+import ListTask from "../ListTask";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 export default function PendingTasks(){
+
+    // Для работы с состоянием
+    const userId = useSelector((state) => state.user.userId);
+    const [arrTasks, setArrTasks] = useState([]);
+    const [refreshMyTasks, setRefreshMyTasks] = useState(0);
+
+    // Функция запрашивает задачи назначеные пользователю
+    async function getData(){
+        await fetch('http://taskmanager/tasks/pending-tasks/' + userId,{method:'get'})
+        .then(resp => resp.json()).then(json => setArrTasks(json) ); 
+    }
+
+    useEffect(()=>{
+        // Если нет данных с сервера. Делаем запрос
+        if(arrTasks.length === 0) getData()
+    }, [userId])
+
+
     return(
         <>
-            PendingTasks
+            <ListTask 
+                arrData={arrTasks   /* Массив данных */} 
+                refreshPerent={()=>setRefreshMyTasks(refreshMyTasks+1)     /* Функция обновления страницы */}/>
         </>
     )
 }
