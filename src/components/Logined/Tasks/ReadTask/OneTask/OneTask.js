a1d09e6b34d3ce900395dc3de89507b455e22129
DimachDim <dakuznecov45@gmail.com>
2024-03-27 20:20:58 +0500

    Обработка удаления задачи и стилизация

--- src/components/Logined/Tasks/ReadTask/OneTask/OneTask.js
+++ src/components/Logined/Tasks/ReadTask/OneTask/OneTask.js
@@ -1,15 +1,26 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import './Style.css'
 
 export default function OneTask(props){
     
-    const {id} = useParams();                       // id переданной задачи
-    const [arrData, setArrData] = useState(null)    // Хранит данные с сервера
+    const {id} = useParams();                               // id переданной задачи
+    const [arrData, setArrData] = useState(null)            // Хранит данные с сервера
+    const userId = useSelector((state)=>state.user.userId)  // id текущего пользователя
+    const navigate = useNavigate()                          // Для управления строкой url
 
     // Запрос задачи с сервера
     async function getTask(){
         await fetch('http://taskmanager/tasks/'+id,{method:'get'})
         .then(resp => resp.json()).then(json => setArrData(json) ); 
+    }
+
+    // Функция удаляет задачу
+    async function deleteTask(){
+        await fetch('http://taskmanager/tasks/' + id,{method:'delete'})
+        // Переходим назад по истории
+        navigate(-1)
     }
 
     useEffect(()=>{
@@ -18,9 +29,25 @@ export default function OneTask(props){
     },[])
 
     console.log(arrData)
-    return(
-        <>
-            OneTask{id}
-        </>
-    )
+    // Если есть данные отрисовываем страницу
+    if(arrData != null){
+        return(
+            <div className="one-task container">
+                <div>{arrData.title}</div>
+                <div dangerouslySetInnerHTML={{__html: arrData.text}}></div>
+                <div>Token: {arrData.token}</div>
+                <div>Статус: {arrData.statusName}</div>
+                <div>Создатель задачи: {arrData.userNameCreator}</div>
+                <div>Назначенный исполнитель: {arrData.userNameExecutor}</div>
+                <div>Дата начала: {arrData.date_start}</div>
+                <div>Дата окончания: {arrData.date_end}</div>
+                
+                {/* Если задача создана вами то рисуем кнопку удалить */}
+                {arrData.id_user_creator === userId
+                    ? <button className="btn btn-danger" onClick={()=>deleteTask()}>Удалить</button>
+                    : ''
+                }
+            </div>
+        )
+    }
 }
