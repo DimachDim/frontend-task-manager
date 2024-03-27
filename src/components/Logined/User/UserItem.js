cbf8c88d93a0905ad45860162dcc74b0768a018d
DimachDim <dakuznecov45@gmail.com>
2024-03-27 11:38:09 +0500

    Стилизация UserItem

--- src/components/Logined/User/UserItem.js
+++ src/components/Logined/User/UserItem.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { useSelector, UseSelector } from "react-redux";
+import './Style.css'
 
 
 export default function UserItem(props){
@@ -9,7 +10,7 @@ export default function UserItem(props){
     const userId2 = props.userId                                // id того с кем проверяем
     const [frendFlag, setFrendFlag] = useState(null)            // Флаг подписаны вы на него или нет
     const [frendFlag2, setFrendFlag2] = useState(null)          // Флаг подписан он на вас или нет
-    const [arrData, setArrData] = useState(null)
+    const [arrData, setArrData] = useState(null)                // Хранит ответ сервера
 
     // Запрашивает данные с сервера
     async function getArrData(){
@@ -32,19 +33,25 @@ export default function UserItem(props){
 
     return(
         <div className='list-group-item'>
-            {props.userName}
+            <div>
+                {props.userName}
+            </div>
 
             {/* Подписан на вас или нет */}
-            {frendFlag2
-                ?'Подписан на вас'
-                :'Не подписан на вас'
-            }
+            <div>
+                {frendFlag2
+                    ?'Подписан на вас'
+                    :'Не подписан на вас'
+                }
+            </div>
+
             {/* Кнопка подписаться */}
-            {frendFlag
-                ?<button>Отписаться</button>
-                :<button>Подписаться</button>
-            }
-            
+            <div>
+                {frendFlag
+                    ?<button className="btn btn-danger">Отписаться</button>
+                    :<button className="btn btn-success">Подписаться</button>
+                } 
+            </div>
         </div>
     )
 }
